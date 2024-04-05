87626e71c60ec6c9d07bfc7692c28e31855af6fc
sidhdhali <sidhnakrani1911@gmail.com>
2024-04-05 14:40:31 +0200

    javascript und required field added

--- newsletter.js
+++ newsletter.js
@@ -1,5 +1,5 @@
 
 
 function newsletter() {
-  confirm("By signing up, you agree to COOK's Privacy Notice and T&Cs.");
+  confirm("By signing up, you agree to COOK's Privacy Notice and T&Cs. You can unsubscribe at any time.");
 }
